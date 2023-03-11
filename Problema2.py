d3d7aa7d9c92d194603fa53768f053858de2ba03
Juan-C13 <clavijuan.eg@gmail.com>
2023-03-11 13:04:55 -0500

    Initialize

---
+++ Problema2.py
@@ -0,0 +1,11 @@
+#Juan Esteban Clavijo Garcia - Codigo: 202225709
+
+pesoT=float(input("Digite su peso en kilogramos:"))
+masa=pesoT/9.8
+pesoM=masa*3.711
+pesoJ=masa*24.79
+pesoL=masa*1.622
+print("")
+print("SU PESO EN MARTE ES",pesoM," kgs")
+print("SU PESO EN JÚPITER ES",pesoJ," kgs")
+print("SU PESO EN LA LUNA ES",pesoL," kgs")
